1d84d84f62785961b9b62d9d5f9f8c1d39595bd2
hamid1882 <hussainhafeez1100@gmail.com>
2023-12-11 11:28:44 +0530

    fix: refactored code

--- src/components/ui/PersonalInformation.js
+++ src/components/ui/PersonalInformation.js
@@ -1,56 +1,48 @@
 import React from "react";
 
+const personal_info_data = [
+  {
+    id: 1,
+    name: "Full Name",
+    placeholder: "Hamid",
+    type: "text",
+  },
+  {
+    id: 2,
+    name: "Gender",
+    placeholder: "Male",
+    type: "text",
+  },
+  {
+    id: 3,
+    name: "Date of Birth",
+    placeholder: "18/07/2002",
+    type: "date",
+  },
+  {
+    id: 4,
+    name: "Phone number",
+    placeholder: "8393434839",
+    type: "number",
+  },
+];
 
-
- function PersonalInformation(){
-    return(
-   <div className="form-box">
-     <div className="profile-label">
-        <label htmlFor="Full name">Full name</label>
-        <input
-          placeholder="Hamid k"
-          className="input"
-          type="text"
-          required
-        />
-      </div>
-
-      <div className="profile-label">
-        <label htmlFor="Gender">Gender</label>
-        <input
-          placeholder="Bride"
-          className="input"
-          type="text"
-          required
-        />
-      </div>
-
-
-      <div className="profile-label">
-        <label htmlFor="Date of birth">Date of birth</label>
-        <input
-          placeholder="Hamid k"
-          className="input"
-          type="text"
-          required
-        />
-      </div>
-
-      <div className="profile-label">
-        <label htmlFor="Phone">Phone number</label>
-        <input
-          placeholder="7789456123"
-          className="input"
-          type="text"
-          required
-        />
-      </div>
-
-      </div>
-    );
-
-
- }
-
+function PersonalInformation() {
+  return (
+    <div className="form-box">
+      {personal_info_data.map((value) => (
+        <div className="profile-label">
+          <label htmlFor={value.name}>{value.name}</label>
+          <input
+            placeholder={value.placeholder}
+            className="input"
+            type={value.type}
+            required
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default PersonalInformation;
